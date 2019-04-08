13d508dfae8346af5b80614baec3d8e388922efb
Вольхин Николай <ulfnew@gmail.com>
2019-04-08 22:29:14 +0500

    Fix syntax errors
     * add index for foreign keys

--- db-part.sql
+++ db-part.sql
@@ -1,5 +1,6 @@
 /*
-1. Я главный редактор‚ у меня есть список новостей. Мне нужно хранить заголовок новости и ее авторов. Предложите мне структуру таблиц.
+1. Я главный редактор‚ у меня есть список новостей. Мне нужно хранить заголовок новости и ее авторов.
+Предложите мне структуру таблиц.
 Количество данных:
 новости ~1 000 000
 авторы ~3 000
@@ -7,16 +8,17 @@
 СУБД PostgreSQL
 */
 SELECT version();
+/* PostgreSQL 9.6.12 on x86_64-pc-mingw64, compiled by gcc.exe (Rev5, Built by MSYS2 project) 4.9.2, 64-bit */
 
 CREATE SCHEMA just_click;
 SET search_path TO just_click;
 SHOW search_path;
 
 CREATE TABLE article
-      (
-      id serial PRIMARY KEY,
-      rating int
-      )
+(
+    id serial PRIMARY KEY,
+    rating int
+)
 ;
 
 CREATE TABLE author
@@ -34,16 +36,20 @@ CREATE TABLE article_author
 COMMENT ON COLUMN just_click.article_author.article_id IS 'link to article';
 COMMENT ON COLUMN just_click.article_author.author_id IS 'link to author of article';
 
+CREATE INDEX article_author_article_id_author_id_index ON article_author (article_id, author_id);
+CREATE INDEX article_author_author_id_article_id_index ON article_author (author_id, article_id);
+
 /*
-2. Вытащите список новостей‚ которые написаны 3-мя и более соавторами. То есть получить отчет «новость — количество соавторов» и отфильтровать те‚ у которых соавторов меньше 3.
+2. Вытащите список новостей‚ которые написаны 3-мя и более соавторами. То есть получить отчет «новость — количество
+соавторов» и отфильтровать те‚ у которых соавторов меньше 3.
 */
 SET search_path TO just_click;
 SHOW search_path;
 
-select 
-       ar.id AS 'новость',
-        count(*) AS 'количество соавторов'
-from 
+select
+       ar.id AS "новость",
+        count(*) AS "количество соавторов"
+from
     article ar
     join article_author aa
     on ar.id = aa.article_id
@@ -61,10 +67,14 @@ SHOW search_path;
 */
 CREATE INDEX article_rating_index ON article (rating);
 
+/*
+В этом запросе можно таблицу author вообще не цеплять, но это у меня в схеме у автора нет ни чего кроме
+идентификатора - в нормальном варианте из таблицы может понадобиться его имя, поэтому таблица участвует в запросе
+ */
 select
-  au.id AS 'автор'
-  , max(ar.rating) AS 'лучшая'
-  , min(ar.rating) AS 'худшая'
+  au.id AS "автор"
+  , max(ar.rating) AS "лучшая"
+  , min(ar.rating) AS "худшая"
 from
     author au
     join article_author aa
@@ -73,3 +83,4 @@ from
     aa.article_id = ar.id
 GROUP BY au.id
 order by au.id
+;
